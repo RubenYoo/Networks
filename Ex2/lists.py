186cb63c2f586e615826430077005e8c4599d16c
RubenYoo <71288728+RubenYoo@users.noreply.github.com>
2021-11-27 21:35:52 +0200

    Create lists.py

---
+++ Ex2/lists.py
@@ -0,0 +1,182 @@
+""" This program has been adapted for use by GVAHIM
+       - the main revisions regard pep8 compliance and use of variable names
+
+Copyright 2010 Google Inc.
+Licensed under the Apache License, Version 2.0
+http://www.apache.org/licenses/LICENSE-2.0
+
+Google's Python Class
+http://code.google.com/edu/languages/google-python-class/
+
+Basic list exercises
+Fill in the code for the functions below. main() is already set up
+to call the functions with a few different inputs,
+printing 'OK' when each function is correct.
+The starter code for each function includes a 'return'
+which is just a placeholder for your code.
+It's ok if you do not complete all the functions, and there
+are some additional functions to try in list2.py. """
+
+
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(words):
+    """
+    @param words: list of strings
+    @return: numbers of words that begin and end with the same char with a length of > 2
+    """
+    counter = 0
+    for i in words:
+        if len(i) > 1:
+            if i[0] == i[-1]:
+                counter += 1
+    return counter
+
+
+# B. front_x
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
+def front_x(words):
+    """
+    @param words: a list of strings
+    @return: a list with the strings
+    in sorted order, except group all the strings that begin with 'x' first.
+    """
+    str1, str2 = [], []
+    for i in words:
+        if len(i) != 0:
+            if i[0] == "x":
+                str1.append(i)
+            else:
+                str2.append(i)
+    str1.sort()
+    str2.sort()
+    return str1 + str2
+
+
+# C. sort_last
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def sort_last(tuples):
+    """
+    @param tuples: a list of tuples
+    @return: a list sorted in increasing order by the last element in each tuple.
+    """
+    tuples.sort(key=lambda tup: tup[-1])
+    return tuples
+
+
+# D. Given a list of numbers, return a list where
+# all adjacent == elements have been reduced to a single element,
+# so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
+# modify the passed in list.
+def remove_adjacent(nums):
+    """
+    @param nums: is a list of nums
+    @return: a list where all adjacent == elements have been reduced to a single element
+    """
+    new_nums = nums[:1]
+    for i in nums[1:]:
+        if i != new_nums[-1]:
+            new_nums.append(i)
+    return new_nums
+
+# E. Given two lists sorted in increasing order, create and return a merged
+# list of all the elements in sorted order. You may modify the passed in lists.
+# Ideally, the solution should work in "linear" time, making a single
+# pass of both lists.
+#
+# NOTE - DO NOT use return sorted(sorted1 + sorted2) - that's too easy :-)
+
+
+def linear_merge(sorted1, sorted2):
+    """
+    @param sorted1: is a sorted list
+    @param sorted2: is a sorted list
+    @return: a sorted list that contain sorted1 and sorted2 (in a linear time)
+    """
+    new_list = []
+    index1, index2 = 0, 0
+
+    while index1 < len(sorted1) and index2 < len(sorted2):
+        if sorted1[index1] > sorted2[index2]:
+            new_list.append(sorted2[index2])
+            index2 += 1
+        else:
+            new_list.append(sorted1[index1])
+            index1 += 1
+
+    while index1 < len(sorted1):
+        new_list.append(sorted1[index1])
+        index1 += 1
+
+    while index2 < len(sorted2):
+        new_list.append(sorted2[index2])
+        index2 += 1
+
+    return new_list
+
+
+def test(got, expected):
+    """ simple test() function used in main() to print
+        what each function returns vs. what it's supposed to return. """
+
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+def main():
+    """ main() calls the above functions with interesting inputs,
+        using test() to check if each result is correct or not. """
+
+    print('\nmatch_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print('\nfront_x')
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print('\nsort_last')
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+    print('\nremove_adjacent')
+    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+    test(remove_adjacent([1, 3, 3, 2, 2, 3]), [1, 3, 2, 3])
+    test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+    test(remove_adjacent([]), [])
+
+    print('\nlinear_merge')
+    test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+         ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+    main()
